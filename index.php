9c14d2a89fe7ef7ac08af42b26a451ac2b96b1b1
Ashek-m <ashekksrct@gmail.com>
2025-01-11 19:38:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" href="Assets/images/logo.png" sizes="32x32">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <title>Home</title>
@@ -961,6 +962,8 @@
         animateCounter('#counter3', 1200, 2500);  // Satisfied Clients
         animateCounter('#counter4', 200, 3500);   // Buildings Constructed
     });
+
+    document.addEventListener('contextmenu', event => event.preventDefault());
 </script>
 
 </body>
